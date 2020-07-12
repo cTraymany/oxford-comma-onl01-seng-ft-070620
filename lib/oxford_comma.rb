ba8de90148106a32a54d7b8a51846f8fae750c59
Chindalath Traymany <chindalatht@gmail.com>
2020-07-12 08:06:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ def oxford_comma(array)
       array.join 
     when 2
       array.join(" and ")
-    when 3
-      array[0..1].join(", ") << ", and #{array[-1]}"
     else
-      array[0..-2].join(", ") << ", and #{array[-1]}"
+      array[0...-1].join(", ") << ", and #{array[-1]}"
   end
 end
